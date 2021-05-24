e313cb4ceeca83049b62daf84b40fbd7fb999109
xDust <leonardoctk3@gmail.com>
2021-05-24 07:54:16 -0300

    padronizando header, footer e background

--- javascript/script.js
+++ javascript/script.js
@@ -8,9 +8,39 @@
                     event.preventDefault()
                     event.stopPropagation()
                 }
-
                 form.classList.add('was-validated')
             }, false)
         })
 })()
+
+var password = document.getElementById("password")
+    , confirm_password = document.getElementById("confirm_password");
+
+
+
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
 
